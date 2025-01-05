66dc685c99a796521f105c7ddfdacc17570b490d
Austin Dragone <18505590+AustinDrag1@users.noreply.github.com>
2025-01-05 16:20:00 -0500

    Wrote flipSign, resetAll, the state functionality, and the number selecting functionality

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+let takingX = true;
+let x = "";
+let y = "";
+let op = null;
+
+// Calculator
+const calculator = document.querySelector("#calculator");
+const screen = document.querySelector(".screen");
+const resetAllBtn = document.querySelector("#AC");
+
+calculator.addEventListener("click", (e) => click(e) );
+resetAllBtn.addEventListener("click", () => resetAll() );
+
+// Checks that click is an acceptable target then delegates what should happen
+function click(e)
+{
+    let selection = e.target.innerText;
+
+    // Number or "." clicked: concat if enough space
+    if( e.target.className.includes("num") )
+    {
+        // Ignore: 10th digit
+        if( screen.innerText.length === 9 ) return;
+
+        // Ignore: second "."
+        if( selection === "." && screen.innerText.includes(".") ) return;
+        
+        // Add digit
+        if( takingX )
+        {
+            x = x + selection;
+            screen.innerText = x;
+        }
+        else
+        {
+            y = y + selection;
+            screen.innerText = y;
+        }
+    }
+    else // Operator clicked: calculate if in correct state
+    {
+        // State irrelevant
+        if( selection === "+/-" ) flipSign();
+        if( selection === "=" ) operate();
+
+        // Requires first state
+
+        // Requires second state
+    }
+}
+
 function add(x,y) { return x+y; }
 function subtract(x,y) { return x-y; }
 function multiply(x,y) { return x*y; }
@@ -6,6 +57,9 @@ function divide(x,y) { return x/y; }
 function operate(x,y,op)
 {
     let result = 0;
+
+    // Validate we're ready to calculate
+    if( x === null || y === null || op == null ) return;
 
     // Send to appropriate method
     switch (op)
@@ -17,15 +71,31 @@ function operate(x,y,op)
     }
 
     // Round to fit screen
-    
 
 }
 
-function parseInput(str)
+function resetAll()
 {
-    // Read from left to right, find 
+    x = "";
+    y = "";
+    screen.innerText = "";
+    takingX = true;
 }
 
-let x = 0;
-let y = 0;
-let op = null;
+function flipSign()
+{
+    let result;
+
+    if( takingX )
+    {
+        x *= -1;
+        result = x;
+    }
+    else
+    {
+        y *= -1;
+        result = y;
+    }
+
+    screen.innerText = result;
+}
